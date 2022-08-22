1bb7f47c0d722d9e6a8bc1f7f7451b2fce578e38
Abdullah <mdabdullah1509@gmail.com>
2022-08-23 01:40:14 +0530

    first commit

---
+++ scrap particular div.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.bls.gov/news.release/laus.nr0.htm"
+
+html_content = requests.get(url).text
+soup = BeautifulSoup(html_content, "html.parser")
+
+texts = soup.find_all('pre')
+for text in texts:
+    print(text.get_text())
